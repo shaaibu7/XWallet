19434010df31c1910ffc4bf46b9f8b7e081ab4d7
shaaibu7 <shaaibusuleiman9@gmail.com>
2025-11-19 11:01:34 +0100

    feat: setup data structures

--- backend/contracts/WalletX.sol
+++ backend/contracts/WalletX.sol
@@ -5,6 +5,38 @@ contract WalletX {
   address tokenAddress;
   address owner;
 
+  struct Wallet {
+        address adminAddress;
+        string walletName;
+        bool active;
+        uint walletId;
+        uint walletBalance;
+        string role;
+    }
+
+    struct WalletMember {
+        address memberAddress;
+        address adminAddress;
+        string organizationName;
+        string name;
+        bool active;
+        uint256 spendLimit; 
+        uint256 memberIdentifier;
+        string role;
+    }
+
+    struct memberTransaction {
+        uint256 amount;
+        address reciever;
+    }
+
+    mapping (address => Wallet) walletAdmin;
+    mapping (address => WalletMember) walletMember;
+
+    mapping (address => WalletMember[]) walletOrganisationMembers;
+    mapping (address => memberTransaction[]) memberTransactions; 
+
+
   constructor(address _tokenAddress) {
     tokenAddress = _tokenAddress;
     owner = msg.sender;
