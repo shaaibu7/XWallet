8d42cd0937987917929c202750ee5d5949457b19
shaaibu7 <shaaibusuleiman9@gmail.com>
2025-11-24 11:55:41 +0100

    chore: add modifier for access control

--- backend/contracts/WalletX.sol
+++ backend/contracts/WalletX.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.28;
 import "./interfaces/IERC20.sol";
 
 contract WalletX {
-  address tokenAddress;
-  address owner;
-  uint256 walletIdTrack = 1;
+    address tokenAddress;
+    address owner;
+    uint256 walletIdTrack = 1;
 
-  struct Wallet {
+    struct Wallet {
         address adminAddress;
         string walletName;
         bool active;
@@ -23,7 +23,7 @@ contract WalletX {
         string organizationName;
         string name;
         bool active;
-        uint256 spendLimit; 
+        uint256 spendLimit;
         uint256 memberIdentifier;
         string role;
     }
@@ -33,26 +33,45 @@ contract WalletX {
         address reciever;
     }
 
-    mapping (address => Wallet) walletAdmin;
-    mapping (address => WalletMember) walletMember;
+    mapping(address => Wallet) walletAdmin;
+    mapping(address => WalletMember) walletMember;
 
-    mapping (address => WalletMember[]) walletOrganisationMembers;
-    mapping (address => memberTransaction[]) memberTransactions; 
+    mapping(address => WalletMember[]) walletOrganisationMembers;
+    mapping(address => memberTransaction[]) memberTransactions;
 
+     modifier onlyAdmin {
+        require(walletAdmin[msg.sender].active == true, "Not a wallet admin account");
+        _;
+    }
 
-  constructor(address _tokenAddress) {
-    tokenAddress = _tokenAddress;
-    owner = msg.sender;
-  }
+    constructor(address _tokenAddress) {
+        tokenAddress = _tokenAddress;
+        owner = msg.sender;
+    }
 
-  function registerWallet(string memory _walletName, uint256 _fundAmount) external {
-        require(walletAdmin[msg.sender].active != true, "Cannot create multiple wallets with one wallet address");
+    function registerWallet(
+        string memory _walletName,
+        uint256 _fundAmount
+    ) external {
+        require(
+            walletAdmin[msg.sender].active != true,
+            "Cannot create multiple wallets with one wallet address"
+        );
 
         // fund escrow with funds
-        uint256 allowance = IERC20(tokenAddress).allowance(msg.sender, address(this));
-        require(allowance >= _fundAmount, "No allowance to spend funds at the moment");
-        IERC20(tokenAddress).transferFrom(msg.sender, address(this), _fundAmount);
-
+        uint256 allowance = IERC20(tokenAddress).allowance(
+            msg.sender,
+            address(this)
+        );
+        require(
+            allowance >= _fundAmount,
+            "No allowance to spend funds at the moment"
+        );
+        IERC20(tokenAddress).transferFrom(
+            msg.sender,
+            address(this),
+            _fundAmount
+        );
 
         Wallet memory walletOrganisation = Wallet({
             adminAddress: msg.sender,
@@ -66,16 +85,16 @@ contract WalletX {
         walletIdTrack += 1;
 
         walletAdmin[msg.sender] = walletOrganisation;
-
     }
-
-
 
     // Getter functions
 
-    function getWalletAdmin() onlyAdmin() external view returns(Wallet memory admin) {
+    function getWalletAdmin()
+        external
+        view
+        onlyAdmin
+        returns (Wallet memory admin)
+    {
         admin = walletAdmin[msg.sender];
     }
-
-
 }
