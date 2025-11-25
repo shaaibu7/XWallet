ca4cb5f1d3986d93f43c7c5bb2bbd2889a3ec892
shaaibu7 <shaaibusuleiman9@gmail.com>
2025-11-25 14:08:51 +0100

    chore: implement page structure

--- backend/ignition/modules/WalletX.ts
+++ backend/ignition/modules/WalletX.ts
@@ -1,9 +1,12 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-export default buildModule("CounterModule", (m) => {
-  const counter = m.contract("Counter");
 
-  m.call(counter, "incBy", [5n]);
 
-  return { counter };
+export default buildModule("WalletXModule", (m) => {
+   const tokenAddress = "0xcebA9300f2b948710d2653dD7B07f33A8B32118C";
+
+  const walletX = m.contract("WalletX", [tokenAddress]);
+ 
+
+  return { walletX };
 });
