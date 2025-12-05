ab07a295b455688c944f3f6257a5dc6fbe8556d5
shaaibu7 <shaaibusuleiman9@gmail.com>
2025-12-05 13:14:55 +0100

    chore: add dependencies for component

---
+++ frontend/vite-project/src/pages/home/Home.jsx
@@ -0,0 +1,5 @@
+import { motion } from "framer-motion";
+import family_budget from "../../assets/family-budget-concept.png";
+import XWalletFooter from "../../layout/component/XWalletFooter";
+import XWalletHeader from "../../layout/component/XWalletHeader";
+import '../../../connection';
