01e7c3912c20f0d1b4d0256e120ee6d79cf51645
shaaibu7 <shaaibusuleiman9@gmail.com>
2025-12-11 09:55:36 +0100

    chore: implement reimburse wallet  hook

---
+++ frontend/vite-project/src/hooks/useReimburseWallet.js
@@ -0,0 +1,73 @@
+import { useCallback } from "react";
+import { toast } from "react-toastify";
+import useContract from "./useContract";
+import useTokenContract from "./useTokenContract";
+import { useAppKitAccount } from "@reown/appkit/react";
+import { useAppKitNetwork } from "@reown/appkit/react";
+import { baseSepolia } from "@reown/appkit/networks";
+import { parseEther, parseUnits } from "ethers";
+
+const useReimburseWallet = () => {
+    const contract = useContract(true);
+    const tokenContract = useTokenContract(true);
+    const { address } = useAppKitAccount();
+    const { chainId } = useAppKitNetwork();
+    return useCallback(
+        async (reimburseAmount) => {
+            if (
+                !reimburseAmount
+            ) {
+                toast.error("Missing field(s)");
+                return;
+            }
+            if (!address) {
+                toast.error("Connect your wallet!");
+                return;
+            }
+            if (Number(chainId) !== baseSepolia.chainId) {
+                toast.error("You are not connected to the right network");
+                return;
+            }
+
+            if (!contract) {
+                toast.error("Cannot get contract!");
+                return;
+            }
+
+            try {
+
+                const parsedPayment = parseUnits(reimburseAmount.toString(), 18);
+
+                if (!tokenContract) {
+                    toast.error("Token contract not initialized. Please reconnect wallet.");
+                    return;
+                }
+
+                const approveToken = await tokenContract.approve("0x2FaFA6557dFf892CB35A8A1024f564C6b0de45D1", parsedPayment);
+
+                const tokenReciept = await approveToken.wait();
+
+                const parsedAmount = BigInt(reimburseAmount);
+
+                const estimatedGas = await contract.reimburseOrganization.estimateGas(
+                    parsedAmount
+                );
+                const tx = await contract.reimburseOrganization(
+                    parsedAmount,
+                    {
+                        gasLimit: (estimatedGas * BigInt(120)) / BigInt(100),
+                    }
+                );
+                
+                return;
+            } catch (error) {
+                console.trace(error)
+                console.error("error while reimbursing wallet: ", error);
+                toast.error("Reimbursing wallet errored");
+            }
+        },
+        [address, chainId, contract]
+    );
+};
+
+export default useReimburseWallet;
