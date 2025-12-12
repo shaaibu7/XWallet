9a05a49384f4a3e2e6a50eb8aefca48441ecd70f
shaaibu7 <shaaibusuleiman9@gmail.com>
2025-12-12 09:05:45 +0100

    chore: implement routes

--- frontend/vite-project/src/router/Router.jsx
+++ frontend/vite-project/src/router/Router.jsx
@@ -16,3 +16,24 @@ import OnboardMembers from "../pages/onboardMembers/OnboardMember";
 import ReimburseOrganization from "../pages/reimburseOrganization/ReimburseOrganization";
 import ReimburseMember from "../pages/reimburseMember/ReimburseMember";
 import Spending from "../pages/spending/Spending";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      {/* LANDING PAGE */}
+      <Route index element={<Home />} />
+
+      {/* EVERY OTHER PAGE ROUTING SHOULD BE DONE IN HERE */}
+      <Route path="/dashboard" element={<DashboardLayout />}>
+        {/* TO NAVIGATE TO THIS ROUTE JUST GO TO: /dashboard */}
+        <Route path="" element={<Dashboard />} />
+        <Route path="register-wallet" element={<RegisterWallet />} />
+        <Route path="onboard-member" element={<OnboardMembers />} />
+        <Route path="reimburse-org" element={<ReimburseOrganization />} />
+        <Route path="reimburse-member" element={<ReimburseMember />} />
+        <Route path="spending" element={<Spending />} />
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Route>
+    </Route>
+  )
+);
