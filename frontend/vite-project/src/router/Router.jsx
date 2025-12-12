243b4616f60c2d67bea14e97b5cdd6350bbeadcd
shaaibu7 <shaaibusuleiman9@gmail.com>
2025-12-12 09:05:21 +0100

    chore: add imports

---
+++ frontend/vite-project/src/router/Router.jsx
@@ -0,0 +1,18 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+// LAYOUTS
+
+// PAGES
+import Home from "../pages/home/Home";
+import DashboardLayout from "../layout/DashboardLayout";
+import Dashboard from "../pages/dashboard/Dashboard";
+import RegisterWallet from "../pages/registerWallet/RegisterWallet";
+import OnboardMembers from "../pages/onboardMembers/OnboardMember";
+import ReimburseOrganization from "../pages/reimburseOrganization/ReimburseOrganization";
+import ReimburseMember from "../pages/reimburseMember/ReimburseMember";
+import Spending from "../pages/spending/Spending";
